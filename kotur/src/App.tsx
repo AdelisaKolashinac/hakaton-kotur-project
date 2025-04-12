f682061c0186599669e7a3b0b227abb0b215a723
Mio <trickovskimio@gmail,com>
2025-04-12 23:30:15 +0200

    Gallery added

--- kotur/src/App.tsx
+++ kotur/src/App.tsx
@@ -1,8 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Gallery from "./components/Gallery/Gallery";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Gallery />
+    </>
+  );
 }
 
 export default App;
