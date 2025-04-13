32920cb9323916fd2c752e8a1a12dec8a048a611
Dijana Galeski <dijana_krsteska@outlook.com>
2025-04-13 11:58:19 +0200

    About us done

--- kotur/src/components/AboutUs/AboutUs.tsx
+++ kotur/src/components/AboutUs/AboutUs.tsx
@@ -1,4 +1,3 @@
-// src/components/AboutUs/AboutUs.tsx
 import React from "react";
 import Header from "../Header/Header";
 import Hero from "./Hero";
@@ -11,6 +10,7 @@ import Team from "./Team";
 import ApplyJob from "./ApplyJob";
 import Testimonials from "../Testimonials/Testimonials";
 import Footer from "../Footer/Footer";
+
 import "./AboutUs.css";
 
 const AboutUs: React.FC = () => {
