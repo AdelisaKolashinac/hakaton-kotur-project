fbf2ad92085de27c51b70c0d7371d40521e5f474
Adelisa Kolashinac <adelisalicina@outlook.com>
2025-04-13 17:17:14 +0200

    test fetch

--- kotur/src/components/Events/Events.tsx
+++ kotur/src/components/Events/Events.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Banner from "./Banner/Banner";
 import Calendar from "./Calendar/Calendar";
 import "./Events.css";
@@ -7,6 +7,22 @@ import EventsCardsList from "../EventsCardsList/EventsCardsList";
 import Footer from "../Footer/Footer";
 import { Link } from "react-router-dom";
 function Events() {
+  useEffect(() => {
+    const fetchedData = async () => {
+      fetch("https://f04d-62-162-133-188.ngrok-free.app/api/events", {
+        headers: {
+          "ngrok-skip-browser-warning": true,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } as any,
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    };
+
+    fetchedData();
+  }, []);
   return (
     <div>
       <Header />
